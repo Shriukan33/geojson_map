6a9a3b06bc54e6288e6b8ca5b50ad8d6adbf6fc6
Benjamin Mourgues <benja.mourgues@hotmail.fr>
2021-05-03 11:20:29 +0200

    Added 0.0.0.0 as allowed host to allow Vagrant to run

--- geojson_map/geojson_map/settings.py
+++ geojson_map/geojson_map/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rt)$pprhbn6$*riv&v%-*n@y8pau4634e7jrq6++!cxv%ydx0g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '0.0.0.0']
 
 
 # Application definition
